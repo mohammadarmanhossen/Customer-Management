86aeed678f79d6b57fe0945640190daf99243917
arman <mdarmanhossen404@gmail.com>
2025-09-22 16:40:23 +0600

    backend setup with PostgreSQL and customer API

---
+++ customer/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=20, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
